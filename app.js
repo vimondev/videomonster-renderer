acf417c661e57746ef523820379ee62e7a8d9600
LUS <jamborl11@gmail.com>
2020-03-17 16:33:18 +0900

    media cache clear

--- app.js
+++ app.js
@@ -47,6 +47,26 @@ async function func() {
       })
     })
   }
+  
+  function DeleteMediaCache() {
+    return new Promise(resolve => {
+        const mediaCacheDir = require('os').homedir() + '\\AppData\\Roaming\\Adobe\\Common\\Media Cache Files'
+        
+        fs.access(mediaCacheDir, err => {
+            if (err) return resolve()
+
+            fs.readdir(mediaCacheDir, (err, files) => {
+                if (err) return resolve()
+
+                files.forEach(file => {
+                    fs.unlinkSync(mediaCacheDir + '\\' + file)
+                })
+
+                resolve()
+            })
+        })
+    })
+}
 
   const config = require(`./config`)
   const video = require(`./modules/video`)
@@ -65,6 +85,8 @@ async function func() {
   }
 
   console.log(`start!`)
+
+  await DeleteMediaCache()
 
   const socket = require(`socket.io-client`)(`http://10.0.0.19:3000`, {
     transports: [`websocket`],

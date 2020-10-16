c815e9e1b469652aa02c7b6a2558266d79f6b72b
adgjl002@naver.com <adgjl002@naer.com>
2020-10-16 14:12:00 +0900

    1. 한글 경로 로그 제거

--- app.js
+++ app.js
@@ -397,14 +397,6 @@ async function func() {
 
       // 오디오 덮어씌우기를 한 경우 페이드인 페이드아웃 처리를 먼저 해준다.
       if (audioReplaceInfo) {
-    
-        const str = Buffer.from(data.audioReplaceInfo.path, `utf-8`)
-        console.log("########### CONVERT TO UTF 8 ###############")
-        console.log(str)
-
-        const str2 = JSON.parse(JSON.stringify(data.audioReplaceInfo.path))
-        console.log(str2)
-    
         // 영상에 유저 오디오를 입힌다.
         const generatedAudioPath = await video.AudioFadeInOut(audioReplaceInfo.path, audioReplaceInfo.StartTime, audioReplaceInfo.FadeDuration, time)
         
